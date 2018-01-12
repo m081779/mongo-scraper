0228b4024d04697ddce6997326b8b210fb6d268b
m081779 <marcoprincipio@hotmail.com>
2018-01-12 02:26:54 -0500

    edited package.json to try and fix heroku deployment

--- server.js
+++ server.js
@@ -12,13 +12,12 @@ const app = express();
 
 const config = require('./config/database');
 mongoose.Promise = Promise;
-mongoose.connect(config.database, err =>{
-  if (err){
-    console.log('Connection failed!');
-  } else {
-    console.log('Connected to MongoDB...');
-  }
-});
+mongoose
+  .connect(config.database)
+  .then((result)=>{
+    console.log(`Connected to database '${result.connections[0].name}' on ${result.connections[0].host}:${result.connections[0].port}`)
+  })
+  .catch(err=> console.log('There was an error with your connection:', err));
 
 
 //setting up Morgan middleware
